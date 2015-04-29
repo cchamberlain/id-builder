19ada27e52fac74e1604896bf3742e7b261b26f6
Tom Wieland <tom@edia.nl>
2015-04-29 20:55:03 +0200

    Added files for all planned tests.

---
+++ src/test/unit/lib/babel.js
@@ -0,0 +1,51 @@
+'use strict';
+
+import log from 'loglevel';
+import { expect } from 'chai';
+import mkdirp from 'mkdirp';
+import rimraf from 'rimraf';
+
+import babel from '../build/../../../lib/babel';
+import { randomString } from '../build/../../../lib/tests';
+
+describe('babel', function() {
+  beforeEach(function (cb) {
+    this.directoryPath = '.tmp/' + randomString();
+    mkdirp(this.directoryPath, cb);
+  });
+
+  afterEach(function (cb) {
+    rimraf(this.directoryPath, cb);
+  });
+
+  describe('sourceExtension', function () {
+    it('should be defined', function (cb) {
+      expect(babel.sourceExtension).to.be.a('string')['with'].length.above(0);
+
+      cb();
+    });
+  });
+
+  describe('targetExtension', function () {
+    it('should be defined', function (cb) {
+      expect(babel.targetExtension).to.be.a('string')['with'].length.above(0);
+
+      cb();
+    });
+  });
+
+  describe('pathReloads(options, path)', function () {
+  });
+
+  describe('sourceFilePathMatches', function () {
+  });
+
+  describe('compileChunk', function() {
+  });
+
+  describe('compileFile', function() {
+  });
+
+  describe('compileAllFiles', function() {
+  });
+});
---
+++ src/test/unit/lib/livescript.js
@@ -0,0 +1,51 @@
+'use strict';
+
+import log from 'loglevel';
+import { expect } from 'chai';
+import mkdirp from 'mkdirp';
+import rimraf from 'rimraf';
+
+import livescript from '../build/../../../lib/livescript';
+import { randomString } from '../build/../../../lib/tests';
+
+describe('livescript', function() {
+  beforeEach(function (cb) {
+    this.directoryPath = '.tmp/' + randomString();
+    mkdirp(this.directoryPath, cb);
+  });
+
+  afterEach(function (cb) {
+    rimraf(this.directoryPath, cb);
+  });
+
+  describe('sourceExtension', function () {
+    it('should be defined', function (cb) {
+      expect(livescript.sourceExtension).to.be.a('string')['with'].length.above(0);
+
+      cb();
+    });
+  });
+
+  describe('targetExtension', function () {
+    it('should be defined', function (cb) {
+      expect(livescript.targetExtension).to.be.a('string')['with'].length.above(0);
+
+      cb();
+    });
+  });
+
+  describe('pathReloads(options, path)', function () {
+  });
+
+  describe('sourceFilePathMatches', function () {
+  });
+
+  describe('compileChunk', function() {
+  });
+
+  describe('compileFile', function() {
+  });
+
+  describe('compileAllFiles', function() {
+  });
+});
