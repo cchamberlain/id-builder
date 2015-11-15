3178b581efc5e9ec1932359bbbb2a488b246b4cb
Tom Wieland <tom.wieland@gmail.com>
2015-11-15 12:55:09 -0500

    updated the packages

--- idBuilder.js
+++ idBuilder.js
@@ -1,6 +1,8 @@
 // Because idBuilder is itself written in ES6, use the babel register require
 // call here to be able to run the ES6 code directly from here.
-require('babel-core/register');
+require('babel-core/register')({
+  presets: [ 'es2015', 'react', 'stage-0', 'stage-1', 'stage-2', 'stage-3' ]
+});
 
 var idBuilder = require('./src');
 
