9deb7baab1577661fccc0d7247ba9db129e3095c
Tom Wieland <tom.wieland@gmail.com>
2015-11-15 13:35:18 -0500

    back to babel-core/register

--- idBuilder.js
+++ idBuilder.js
@@ -1,6 +1,6 @@
 // Because idBuilder is itself written in ES6, use the babel register require
 // call here to be able to run the ES6 code directly from here.
-require('babel-register')({
+require('babel-core/register')({
   presets: [ 'es2015', 'react', 'stage-0' ]
 });
 
