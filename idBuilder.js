430ed0f1530840992f96c8682a06689a3c92b9ab
Tom Wieland <tom.wieland@gmail.com>
2015-11-15 13:16:13 -0500

    trying babel-runtime module

--- idBuilder.js
+++ idBuilder.js
@@ -1,7 +1,7 @@
 // Because idBuilder is itself written in ES6, use the babel register require
 // call here to be able to run the ES6 code directly from here.
-require('babel-core/register')({
-  presets: [ 'es2015', 'react', 'stage-0', 'stage-1', 'stage-2', 'stage-3' ]
+require('babel-register')({
+  presets: [ 'es2015', 'react', 'stage-0' ]
 });
 
 var idBuilder = require('./src');
